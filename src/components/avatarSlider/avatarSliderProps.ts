7137aae9cc321ce158cb855978d2398c08721666
Sahil Memon <140768015+memonsahil@users.noreply.github.com>
2024-01-14 20:50:54 +0000

    Simplify by removing redundant components

--- src/components/avatarSlider/avatarSliderProps.ts
+++
@@ -1,6 +0,0 @@
-type avatarSliderProps = {
-    avatars: { uri: string }[]
-    currentAvatar: { uri: string }
-}
-
-export default avatarSliderProps
--- src/components/header/headerProps.ts
+++
@@ -1,5 +0,0 @@
-type headerProps = {
-    title: string
-}
-
-export default headerProps
