b1646f898b42df474ce7f84faee21d5e1893069f
Sahil Memon <140768015+memonsahil@users.noreply.github.com>
2024-01-14 07:05:25 +0000

    Add fontSizes and themeColors enums

---
+++ src/enums/fontSizes.ts
@@ -0,0 +1,7 @@
+enum fontSizes {
+    TITLE = 20,
+    HEADING = 26,
+    SUB_HEADING = 22,
+}
+
+export default fontSizes
---
+++ src/enums/themeColors.ts
@@ -0,0 +1,8 @@
+enum themeColors {
+    BACKGROUND_PRIMARY = '#E8E9ED',
+    BACKGROUND_SECONDARY = '#0000ff',
+    TEXT_PRIMARY = '#00ff00',
+    TEXT_SECONDARY = '#ffff00',
+}
+
+export default themeColors
