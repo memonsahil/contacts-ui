55a2d9659ac7299b6cf255ce6bc6f7a769d3de50
Sahil Memon <140768015+memonsahil@users.noreply.github.com>
2024-01-15 04:34:51 +0000

    Create contactType, format contacts

---
+++ src/data/contactType.ts
@@ -0,0 +1,10 @@
+type contactType = {
+    userId: string
+    firstName: string
+    lastName: string
+    avatar: { uri: string }
+    occupation: string
+    bio: string
+}
+
+export default contactType
