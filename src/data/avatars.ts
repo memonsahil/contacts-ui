edf5a6e1c7bc115e44553478c31b37de9d423543
Sahil Memon <140768015+memonsahil@users.noreply.github.com>
2024-01-15 05:57:57 +0000

    Import avatars and create types

---
+++ src/data/avatars.ts
@@ -0,0 +1,61 @@
+import avatar_1 from '../assets/avatar_1.png'
+import avatar_2 from '../assets/avatar_2.png'
+import avatar_3 from '../assets/avatar_3.png'
+import avatar_4 from '../assets/avatar_4.png'
+import avatar_5 from '../assets/avatar_5.png'
+import avatar_6 from '../assets/avatar_6.png'
+import avatar_7 from '../assets/avatar_7.png'
+import avatar_8 from '../assets/avatar_8.png'
+import avatar_9 from '../assets/avatar_9.png'
+import avatar_10 from '../assets/avatar_10.png'
+import avatar_11 from '../assets/avatar_11.png'
+import avatar_12 from '../assets/avatar_12.png'
+import avatar_13 from '../assets/avatar_13.png'
+import avatar_14 from '../assets/avatar_14.png'
+import avatar_15 from '../assets/avatar_15.png'
+import avatar_16 from '../assets/avatar_16.png'
+import avatar_17 from '../assets/avatar_17.png'
+import avatar_18 from '../assets/avatar_18.png'
+import avatar_19 from '../assets/avatar_19.png'
+import avatar_20 from '../assets/avatar_20.png'
+import avatar_21 from '../assets/avatar_21.png'
+import avatar_22 from '../assets/avatar_22.png'
+import avatar_23 from '../assets/avatar_23.png'
+import avatar_24 from '../assets/avatar_24.png'
+import avatar_25 from '../assets/avatar_25.png'
+import avatar_26 from '../assets/avatar_26.png'
+import avatar_27 from '../assets/avatar_27.png'
+import avatar_28 from '../assets/avatar_28.png'
+
+const avatars = [
+    avatar_1,
+    avatar_2,
+    avatar_3,
+    avatar_4,
+    avatar_5,
+    avatar_6,
+    avatar_7,
+    avatar_8,
+    avatar_9,
+    avatar_10,
+    avatar_11,
+    avatar_12,
+    avatar_13,
+    avatar_14,
+    avatar_15,
+    avatar_16,
+    avatar_17,
+    avatar_18,
+    avatar_19,
+    avatar_20,
+    avatar_21,
+    avatar_22,
+    avatar_23,
+    avatar_24,
+    avatar_25,
+    avatar_26,
+    avatar_27,
+    avatar_28,
+]
+
+export default avatars
---
+++ src/types/contactType.ts
@@ -0,0 +1,9 @@
+type contactType = {
+    userId: string
+    firstName: string
+    lastName: string
+    occupation: string
+    bio: string
+}
+
+export default contactType
